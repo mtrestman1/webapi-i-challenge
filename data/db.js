4c7b1c5c94ccdcc38c36ef8e4a162c80e5f7b6cd
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-03-18 15:19:23 -0400

    webapi-i-challenge

--- data/db.js
+++ data/db.js
@@ -10,6 +10,8 @@ module.exports = {
   remove,
 };
 
+
+
 function find() {
   return db('users');
 }
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+const server = express();
+server.use(express.json());
+
+
+
+
+server.listen(4000, () => {
+    console.log('/n** API up and running on Port 4000**')
+})
